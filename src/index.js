1a3b7ea5a7640d9b9347ad27ce46c252961d3e9d
Brittany Blake <brittany.blake49@gmail.com>
2020-08-28 12:34:06 +0200

    add local storale storage

--- src/index.js
+++ src/index.js
@@ -1,71 +1,72 @@
-import render from '../interface';
-import './style.css';
+import render from "../interface";
+import "./style.css";
 
 render();
-const form = document.querySelector('#addTask');
-const projectForm = document.querySelector('#projectForm');
-const addTaskBtn = document.querySelector('.addtaskbtn');
-const submitTaskBtn = document.querySelector('.submittaskbtn');
-const addProjectBtn = document.querySelector('#addProjectBtn');
-const generatedProjectItems = document.querySelector('.Generateditem');
-const taskForm = document.querySelector('.tables');
-const input1 = document.querySelector('.input1');
-const input2 = document.querySelector('.input2');
-const input3 = document.querySelector('.input3');
-const tableButton = document.querySelector('.delete');
-const options = document.querySelector('.options');
-const deletetable = document.querySelector('.removeButton');
+const form = document.querySelector("#addTask");
+const projectForm = document.querySelector("#projectForm");
+const addTaskBtn = document.querySelector(".addtaskbtn");
+const submitTaskBtn = document.querySelector(".submittaskbtn");
+const addProjectBtn = document.querySelector("#addProjectBtn");
+const generatedProjectItems = document.querySelector(".Generateditem");
+const taskForm = document.querySelector(".tables");
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
+const input3 = document.querySelector(".input3");
+const tableButton = document.querySelector(".delete");
+const options = document.querySelector(".options");
+const deletetable = document.querySelector(".removeButton");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
-deletetable.addEventListener('click', (e) => {
+deletetable.addEventListener("click", (e) => {
   const deleteProject = e.target.parentElement;
   deleteProject.remove();
 });
 
 const hideForm = () => {
-  form.classList.add('hidden');
+  form.classList.add("hidden");
 };
 
 const showForm = () => {
-  form.classList.remove('hidden');
+  form.classList.remove("hidden");
 };
 
-addTaskBtn.addEventListener('click', (e) => {
+addTaskBtn.addEventListener("click", (e) => {
   showForm();
 });
 
-options.addEventListener('click', (e) => {
+options.addEventListener("click", (e) => {
   const target = e.target.id;
 });
 
 function getpriority() {
-  if (document.querySelector('#high').checked) {
-    return 'high';
+  if (document.querySelector("#high").checked) {
+    return "high";
   }
-  if (document.querySelector('#medium').checked) {
-    return 'medium';
+  if (document.querySelector("#medium").checked) {
+    return "medium";
   }
-  if (document.querySelector('#low').checked) {
-    return 'low';
+  if (document.querySelector("#low").checked) {
+    return "low";
   }
-  return 'no priority';
+  return "no priority";
 }
 
-submitTaskBtn.addEventListener('click', (e) => {
+submitTaskBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const newcheckbox = document.createElement('input');
-  newcheckbox.setAttribute('type', 'checkbox');
-  const newrow = document.createElement('tr');
-  newrow.id = 'newproject';
-  const formTitle = document.createElement('td');
-  const formDescription = document.createElement('td');
-  const formDate = document.createElement('td');
-  const formcompletion = document.createElement('td');
-  const formpriority = document.createElement('td');
-  const removebutton = document.createElement('td');
-  const tableButton = document.createElement('button');
-  tableButton.innerHTML = 'X';
-  tableButton.className = 'delete';
-  formpriority.id = 'newpriority';
+  const newcheckbox = document.createElement("input");
+  newcheckbox.setAttribute("type", "checkbox");
+  const newrow = document.createElement("tr");
+  newrow.id = "newproject";
+  const formTitle = document.createElement("td");
+  const formDescription = document.createElement("td");
+  const formDate = document.createElement("td");
+  const formcompletion = document.createElement("td");
+  const formpriority = document.createElement("td");
+  const removebutton = document.createElement("td");
+  const tableButton = document.createElement("button");
+  tableButton.innerHTML = "X";
+  tableButton.className = "delete";
+  formpriority.id = "newpriority";
   formpriority.innerHTML = getpriority();
   formTitle.innerHTML = input1.value;
   formDescription.innerHTML = input2.value;
@@ -79,17 +80,17 @@ submitTaskBtn.addEventListener('click', (e) => {
   newrow.appendChild(formpriority);
   newrow.appendChild(removebutton);
   taskForm.appendChild(newrow);
-  if (formpriority.innerHTML === 'high') {
-    formpriority.classList.add('high');
-  } else if (formpriority.innerHTML === 'medium') {
-    formpriority.classList.add('medium');
-  } else if (formpriority.innerHTML === 'low') {
-    formpriority.classList.add('low');
+  if (formpriority.innerHTML === "high") {
+    formpriority.classList.add("high");
+  } else if (formpriority.innerHTML === "medium") {
+    formpriority.classList.add("medium");
+  } else if (formpriority.innerHTML === "low") {
+    formpriority.classList.add("low");
   } else {
-    formpriority.classList.add('no-priority');
+    formpriority.classList.add("no-priority");
   }
 
-  tableButton.addEventListener('click', (e) => {
+  tableButton.addEventListener("click", (e) => {
     const tableRow = e.target.parentElement.parentElement;
     tableRow.remove();
   });
@@ -97,164 +98,164 @@ submitTaskBtn.addEventListener('click', (e) => {
   hideForm();
 });
 
-addProjectBtn.addEventListener('click', (e) => {
+addProjectBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const generatedDiv = document.createElement('div');
-  generatedDiv.className = 'item';
-  const generatedName = document.createElement('h4');
-  const newaddition = document.createElement('button');
-  newaddition.innerHTML = 'Add';
-  newaddition.className = 'editButton addtaskbtn';
-  const newdeletion = document.createElement('button');
-  newdeletion.innerHTML = 'Remove';
-  newdeletion.className = 'removeButton';
-  generatedName.className = 'item-input';
+  const generatedDiv = document.createElement("div");
+  generatedDiv.className = "item";
+  const generatedName = document.createElement("h4");
+  const newaddition = document.createElement("button");
+  newaddition.innerHTML = "Add";
+  newaddition.className = "editButton addtaskbtn";
+  const newdeletion = document.createElement("button");
+  newdeletion.innerHTML = "Remove";
+  newdeletion.className = "removeButton";
+  generatedName.className = "item-input";
 
-  newdeletion.addEventListener('click', (e) => {
+  newdeletion.addEventListener("click", (e) => {
     const deleteProject = e.target.parentElement;
     deleteProject.remove();
   });
 
-  const newform = document.createElement('form');
-  newform.className = 'hidden';
+  const newform = document.createElement("form");
+  newform.className = "hidden";
 
-  const titlediv = document.createElement('div');
-  const titlelabel = document.createElement('label');
-  const titleinput = document.createElement('input');
-  titlelabel.innerHTML = 'Title';
+  const titlediv = document.createElement("div");
+  const titlelabel = document.createElement("label");
+  const titleinput = document.createElement("input");
+  titlelabel.innerHTML = "Title";
 
-  const descdiv = document.createElement('div');
-  const desclabel = document.createElement('label');
-  const descinput = document.createElement('input');
-  desclabel.innerHTML = 'Description';
+  const descdiv = document.createElement("div");
+  const desclabel = document.createElement("label");
+  const descinput = document.createElement("input");
+  desclabel.innerHTML = "Description";
 
-  const datediv = document.createElement('div');
-  const datelabel = document.createElement('label');
-  const dateinput = document.createElement('input');
-  datelabel.innerHTML = 'Due date';
+  const datediv = document.createElement("div");
+  const datelabel = document.createElement("label");
+  const dateinput = document.createElement("input");
+  datelabel.innerHTML = "Due date";
 
   generatedName.innerHTML = projectForm.value;
 
-  const newoptions = document.createElement('div');
-  newoptions.className = 'options';
+  const newoptions = document.createElement("div");
+  newoptions.className = "options";
 
-  const newAddDiv = document.createElement('div');
-  const newAddButton = document.createElement('button');
-  newAddButton.className = 'add submittaskbtn';
-  newAddButton.innerHTML = 'Add to Project';
+  const newAddDiv = document.createElement("div");
+  const newAddButton = document.createElement("button");
+  newAddButton.className = "add submittaskbtn";
+  newAddButton.innerHTML = "Add to Project";
 
-  const highdiv = document.createElement('div');
-  const highlabel = document.createElement('label');
-  highlabel.setAttribute('for', 'high');
-  highlabel.innerHTML = 'High';
-  const highinput = document.createElement('input');
-  highinput.setAttribute('type', 'radio');
+  const highdiv = document.createElement("div");
+  const highlabel = document.createElement("label");
+  highlabel.setAttribute("for", "high");
+  highlabel.innerHTML = "High";
+  const highinput = document.createElement("input");
+  highinput.setAttribute("type", "radio");
   // highinput.setAttribute('id', 'high');
-  highinput.className = 'high';
-  highinput.setAttribute('value', 'impact');
-  highinput.setAttribute('name', 'priority');
+  highinput.className = "high";
+  highinput.setAttribute("value", "impact");
+  highinput.setAttribute("name", "priority");
 
-  const mediumdiv = document.createElement('div');
-  const mediumlabel = document.createElement('label');
-  mediumlabel.setAttribute('for', 'medium');
-  mediumlabel.innerHTML = 'medium';
-  const mediuminput = document.createElement('input');
-  mediuminput.setAttribute('type', 'radio');
+  const mediumdiv = document.createElement("div");
+  const mediumlabel = document.createElement("label");
+  mediumlabel.setAttribute("for", "medium");
+  mediumlabel.innerHTML = "medium";
+  const mediuminput = document.createElement("input");
+  mediuminput.setAttribute("type", "radio");
   // mediuminput.setAttribute('id', 'medium');
-  mediuminput.setAttribute('value', 'impact2');
-  mediuminput.setAttribute('name', 'priority');
+  mediuminput.setAttribute("value", "impact2");
+  mediuminput.setAttribute("name", "priority");
 
-  const lowdiv = document.createElement('div');
-  lowdiv.className = 'lowdiv';
-  const lowlabel = document.createElement('label');
-  lowlabel.setAttribute('for', 'low');
-  lowlabel.innerHTML = 'low';
-  const lowinput = document.createElement('input');
-  lowinput.setAttribute('type', 'radio');
+  const lowdiv = document.createElement("div");
+  lowdiv.className = "lowdiv";
+  const lowlabel = document.createElement("label");
+  lowlabel.setAttribute("for", "low");
+  lowlabel.innerHTML = "low";
+  const lowinput = document.createElement("input");
+  lowinput.setAttribute("type", "radio");
   // lowinput.setAttribute('id', 'low');
   // lowinput.id = 'low';
-  lowinput.className = 'low';
-  lowinput.setAttribute('value', 'impact1');
-  lowinput.setAttribute('name', 'priority');
+  lowinput.className = "low";
+  lowinput.setAttribute("value", "impact1");
+  lowinput.setAttribute("name", "priority");
 
-  const table = document.createElement('table');
-  table.className = 'tables';
-  const tableHead = document.createElement('thead');
-  const tableRow = document.createElement('tr');
-  const tableTitle = document.createElement('th');
-  tableTitle.innerHTML = 'Title';
-  const tableDesc = document.createElement('th');
-  tableDesc.innerHTML = 'Description';
-  const tableDate = document.createElement('th');
-  tableDate.innerHTML = 'Due date';
-  const tableCompleted = document.createElement('th');
-  tableCompleted.innerHTML = 'completed?';
-  const tablePriority = document.createElement('th');
-  tablePriority.innerHTML = 'priority';
-  const tableRemove = document.createElement('th');
-  tableRemove.innerHTML = 'remove';
+  const table = document.createElement("table");
+  table.className = "tables";
+  const tableHead = document.createElement("thead");
+  const tableRow = document.createElement("tr");
+  const tableTitle = document.createElement("th");
+  tableTitle.innerHTML = "Title";
+  const tableDesc = document.createElement("th");
+  tableDesc.innerHTML = "Description";
+  const tableDate = document.createElement("th");
+  tableDate.innerHTML = "Due date";
+  const tableCompleted = document.createElement("th");
+  tableCompleted.innerHTML = "completed?";
+  const tablePriority = document.createElement("th");
+  tablePriority.innerHTML = "priority";
+  const tableRemove = document.createElement("th");
+  tableRemove.innerHTML = "remove";
 
-  newaddition.addEventListener('click', (e) => {
+  newaddition.addEventListener("click", (e) => {
     e.preventDefault();
-    newform.classList.remove('hidden');
+    newform.classList.remove("hidden");
   });
 
-  newAddButton.addEventListener('click', (e) => {
+  newAddButton.addEventListener("click", (e) => {
     e.preventDefault();
-    projectForm.value = '';
-    newform.classList.add("hidden");;
+    projectForm.value = "";
+    newform.classList.add("hidden");
 
     function getpriority1() {
       if (highinput.checked) {
-        return 'high';
+        return "high";
       }
       if (mediuminput.checked) {
-        return 'medium';
+        return "medium";
       }
       if (lowinput.checked) {
-        return 'low';
+        return "low";
       }
-      return 'no priority';
+      return "no priority";
     }
 
-    const tableBody = document.createElement('tbody');
-    const bodyRow = document.createElement('tr');
-    const titleData = document.createElement('td');
+    const tableBody = document.createElement("tbody");
+    const bodyRow = document.createElement("tr");
+    const titleData = document.createElement("td");
     titleData.innerHTML = titleinput.value;
-    const descData = document.createElement('td');
+    const descData = document.createElement("td");
     descData.innerHTML = descinput.value;
-    const timeData = document.createElement('td');
+    const timeData = document.createElement("td");
     timeData.innerHTML = dateinput.value;
-    const completedData = document.createElement('td');
+    const completedData = document.createElement("td");
     completedData.innerHTML = getpriority1();
-    completedData.className = 'high';
-    const priorityData = document.createElement('td');
-    const tableInput = document.createElement('input');
-    const deleteTable = document.createElement('td');
-    tableInput.setAttribute('type', 'checkbox');
-    const tableButton = document.createElement('button');
-    tableButton.className = 'delete';
-    tableButton.innerHTML = 'X';
-    const newcheckbox = document.createElement('input');
-    newcheckbox.setAttribute('type', 'checkbox');
+    completedData.className = "high";
+    const priorityData = document.createElement("td");
+    const tableInput = document.createElement("input");
+    const deleteTable = document.createElement("td");
+    tableInput.setAttribute("type", "checkbox");
+    const tableButton = document.createElement("button");
+    tableButton.className = "delete";
+    tableButton.innerHTML = "X";
+    const newcheckbox = document.createElement("input");
+    newcheckbox.setAttribute("type", "checkbox");
     priorityData.appendChild(newcheckbox);
     deleteTable.appendChild(tableButton);
     newform.reset();
 
-    deleteTable.addEventListener('click', (e) => {
+    deleteTable.addEventListener("click", (e) => {
       const tableRow = e.target.parentElement.parentElement;
       tableRow.remove();
     });
 
-    if (completedData.innerHTML === 'high') {
-      completedData.classList.add('high');
-    } else if (completedData.innerHTML === 'medium') {
-      completedData.classList.add('medium');
-    } else if (completedData.innerHTML === 'low') {
-      completedData.classList.add('low');
+    if (completedData.innerHTML === "high") {
+      completedData.classList.add("high");
+    } else if (completedData.innerHTML === "medium") {
+      completedData.classList.add("medium");
+    } else if (completedData.innerHTML === "low") {
+      completedData.classList.add("low");
     } else {
-      completedData.classList.add('no-priority');
+      completedData.classList.add("no-priority");
     }
 
     tableRow.appendChild(tableTitle);
@@ -274,6 +275,47 @@ addProjectBtn.addEventListener('click', (e) => {
     table.appendChild(tableHead);
     table.appendChild(tableBody);
     generatedDiv.appendChild(table);
+
+    const result = new item(
+      titleData.innerHTML,
+      descData.innerHTML,
+      timeData.innerHTML,
+      completedData.innerHTML
+    );
+
+    items.push(result);
+    console.log(items);
+    localStorage.setItem("items", JSON.stringify(items));
+
+    items.forEach((item) => {
+      const storageRow = document.createElement("tr");
+      const storageTitle = document.createElement("td");
+      storageTitle.innerHTML = item.title;
+      const storageDesc = document.createElement("td");
+      storageDesc.innerHTML = item.description;
+      const storageDate = document.createElement("td");
+      storageDate.innerHTML = item.dueDate;
+      const storagePriority = document.createElement("td");
+      storagePriority.innerHTML = item.complete;
+      const storageCheckbox = document.createElement("td");
+      const storageCheckInput = document.createElement("input");
+      storageCheckInput.setAttribute("type", "checkbox");
+      const storageDeleteTable = document.createElement("td");
+      const storageTableButton = document.createElement("button");
+      storageTableButton.className = "delete";
+      storageTableButton.innerHTML = "X";
+
+
+
+
+
+
+
+
+    })
+
+
+
   });
 
   // TABLE BODY AND DATA
@@ -316,15 +358,15 @@ addProjectBtn.addEventListener('click', (e) => {
   generatedDiv.appendChild(newform);
 });
 
-tableButton.addEventListener('click', (e) => {
+tableButton.addEventListener("click", (e) => {
   const tableRow = e.target.parentElement.parentElement;
   tableRow.remove();
 });
 
-const item = (title, description, dueDate, complete, priority) => ({
-  title,
-  description,
-  dueDate,
-  complete,
-  priority,
-});
+function item(title, description, dueDate, complete = "", priority = "") {
+  (this.title = title),
+    (this.description = description),
+    (this.dueDate = dueDate),
+    (this.complete = complete),
+    (this.priority = priority);
+}
